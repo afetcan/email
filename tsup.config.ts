fb5f0c90d047cfc0258df200ff8e2d79d71f0013
productdevbook <hi@productdevbook.com>
2023-02-09 17:28:33 +0300

    fix: export all ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -12,13 +12,14 @@ import type { Options } from 'tsup'
 // ]
 
 export default <Options>{
-  entryPoints: ['src/index.ts'],
+  entryPoints: ['src/*'],
   outDir: 'dist',
   target: 'node18', // needed for working ESM
   format: ['esm'],
   splitting: false,
   sourcemap: true,
   clean: true,
+  dts: true,
   minify: process.env.NODE_ENV === 'production',
   // noExternal,
   skipNodeModulesBundle: true,
